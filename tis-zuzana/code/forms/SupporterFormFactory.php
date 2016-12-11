8c306f2e3707fac113be1928ed456b950415b280
Filip Likavčan <filip@bratia.sk>
2016-12-11 13:42:40 +0100

    Implements form errors

--- tis-zuzana/code/forms/SupporterFormFactory.php
+++ tis-zuzana/code/forms/SupporterFormFactory.php
@@ -7,10 +7,10 @@ class SupporterFormFactory extends BaseFormFactory
 
         $form->addText('Email', 'Email*')
             ->addRule(Application\Form::EMAIL, 'A valid emaill address is required.')
-            ->setRequired(true);
-        $form->addText('FirstName', 'First Name');
-        $form->addText('LastName', 'Last Name');
-        $form->addText('City', 'City');
+            ->addRule(\Nette\Forms\Form::REQUIRED, 'Tento údaj je povinný.');
+        $form->addText('FirstName', 'Meno');
+        $form->addText('LastName', 'Priezvisko');
+        $form->addText('City', 'Mesto');
         $form->addCheckbox('Hide', 'Nechcem, aby bolo moje meno zverejnené');
 
         $submit = $form->addSubmit('submit', 'Odoslať');
@@ -51,7 +51,7 @@ class SupporterFormFactory extends BaseFormFactory
         else
         {
             $form->cleanErrors();
-            $form->addError('Please make sure all fields are filled out properly.');
+            $form->addError('Zadajte aspoň email, prosím.');
         }
     }
 }
