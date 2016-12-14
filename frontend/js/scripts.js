061e8304d772bfb69a7a89573cfb213f26d4e6fb
Michal Chovaňák <michal.chovanak@gmail.com>
2016-12-14 15:51:28 +0100

    changes in styles, colors, ...

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,29 +1,34 @@
-jQuery( document ).ready(function( $ ) {
-  $('.topbar').on('click', '.js-open-menu', function(event){
-    event.preventDefault();
-    $(this).parent().next('.nav-wrapper').addClass('is-opened');
-  });
-  $('.topbar').on('click', '.js-close-menu', function(event){
-    event.preventDefault();
-    $(this).parent().removeClass('is-opened');
-  });
-  $('input[name=Country]').each(function(){
-      var input = $(this);
+jQuery(document).ready(function($) {
+	$('.topbar').on('click', '.js-open-menu', function(event) {
+		event.preventDefault();
+		$(this).parent().next('.nav-wrapper').addClass('is-opened');
+	});
 
-      var select = $('<select name="Country"></select>');
+	$('.topbar').on('click', '.js-close-menu', function(event) {
+		event.preventDefault();
+		$(this).parent().removeClass('is-opened');
+	});
 
-      $.each(COUNTRIES.sk, function(code, name) {
-          select.append('<option value="' + code + '"' + (code == 'SK' ? ' selected="selected"' : '') + '>' + name + '</option>');
-      });
+	$('input[name=Country]').each(function() {
+		var input = $(this);
 
-      input.replaceWith(select);
-  });
-  if(!$("#teasers-list").length){
-    return;
-  }
-  $('.teasers-list').masonry({
-  // options
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer'
-  });
+		var select = $('<select name="Country"></select>');
+
+		$.each(COUNTRIES.sk, function(code, name) {
+			select.append('<option value="' + code + '"' + (code == 'SK' ? ' selected="selected"' : '') + '>' + name + '</option>');
+		});
+
+		input.replaceWith(select);
+	});
+
+	if (!$("#teasers-list").length) {
+		return;
+	}
+
+	$('.teasers-list').masonry({
+		// options
+		itemSelector: '.grid-item',
+		columnWidth: '.grid-sizer'
+	});
+
 });
