7e3d15e69148dc6e032851cc4b4a8f8d793bafc4
Samuel Snopko <sas@gpm-agency.net>
2016-12-14 15:41:14 +0100

    fixed menu if is clicked section link - menu close

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,81 +1,79 @@
-jQuery( document ).ready(function( $ ) {
+jQuery(document).ready(function($) {
 
-    var last_supporter_id = $('.people-list li:last').data('id');
+	var last_supporter_id = $('.people-list li:last').data('id');
 
-    function loadSupporters()
-    {
-        var button = $(this);
+	function loadSupporters() {
+		var button = $(this);
 
-        if (!button.hasClass('disabled'))
-        {
-            button.addClass('disabled').text(button.data('loading'));
+		if(!button.hasClass('disabled')) {
+			button.addClass('disabled').text(button.data('loading'));
 
-            $.getJSON(button.attr('href'), { i: last_supporter_id }).always(function(response) {
-                button.attr('href', response.Supporters.LoadMoreLink);
-                button.removeClass('disabled').text(button.data('loaded'));
+			$.getJSON(button.attr('href'), {
+				i: last_supporter_id
+			}).always(function(response) {
+				button.attr('href', response.Supporters.LoadMoreLink);
+				button.removeClass('disabled').text(button.data('loaded'));
 
-                var count = response.Supporters.Supporters.length;
+				var count = response.Supporters.Supporters.length;
 
-                if (count < response.Supporters.PageSize)
-                {
-                    button.remove();
-                }
+				if(count < response.Supporters.PageSize) {
+					button.remove();
+				}
 
-                if (count > 0)
-                {
-                    var last_id, html, list = $('.people-list:first');
+				if(count > 0) {
+					var last_id, html, list = $('.people-list:first');
 
-                    $.each(response.Supporters.Supporters, function(i, supporter) {
-                        html = '<li><p><strong>' + supporter.Name + '</strong>';
+					$.each(response.Supporters.Supporters, function(i, supporter) {
+						html = '<li><p><strong>' + supporter.Name + '</strong>';
 
-                        if (supporter.Country)
-                        {
-                            html += ', ' + supporter.Country;
-                        }
+						if(supporter.Country) {
+							html += ', ' + supporter.Country;
+						}
 
-                        last_supporter_id = supporter.ID;
+						last_supporter_id = supporter.ID;
 
-                        list.append(html);
-                    });
-                }
-            });
-        }
-    }
+						list.append(html);
+					});
+				}
+			});
+		}
+	}
 
-    $('body')
-        .on('click', '.js-open-menu', function(event){
-            event.preventDefault();
-            $(this).parent().next('.nav-wrapper').addClass('is-opened');
-        })
-        .on('click', '.js-close-menu', function(event){
-            event.preventDefault();
-            $(this).parent().removeClass('is-opened');
-        }).on('click', '.show-more-supporters', function(event) {
-            event.preventDefault();
-            loadSupporters.call(this);
-        });
-
-    $('input[name=Country]').each(function(){
-        var input = $(this);
-        var value = input.val();
-
-        var select = $('<select name="Country"></select>');
-
-        $.each(COUNTRIES.sk, function(code, name) {
-            select.append('<option value="' + code + '"' + (code == value ? ' selected="selected"' : '') + '>' + name + '</option>');
-        });
-
-        input.replaceWith(select);
+	$('body')
+		.on('click', '.js-open-menu', function(event) {
+			event.preventDefault();
+			$(this).parent().next('.nav-wrapper').addClass('is-opened');
+		})
+		.on('click', '.js-close-menu', function(event) {
+			event.preventDefault();
+			$(this).parent().removeClass('is-opened');
+		}).on('click', '.show-more-supporters', function(event) {
+			event.preventDefault();
+			loadSupporters.call(this);
+		}).on('click', '.menu-main a', function(){
+      $(this).closest('.nav-wrapper').removeClass('is-opened');
     });
 
-    if(!$("#teasers-list").length)
-    {
-        return;
-    }
+	$('input[name=Country]').each(function() {
+		var input = $(this);
+		var value = input.val();
 
-    $('.teasers-list').masonry({
-        // options
-        itemSelector: '.grid-item',
-        columnWidth: '.grid-sizer'
-    });
+		var select = $('<select name="Country"></select>');
+
+		$.each(COUNTRIES.sk, function(code, name) {
+			select.append('<option value="' + code + '"' + (code == value ? ' selected="selected"' : '') + '>' + name + '</option>');
+		});
+
+		input.replaceWith(select);
+	});
+
+	if(!$("#teasers-list").length) {
+		return;
+	}
+
+	$('.teasers-list').masonry({
+		// options
+		itemSelector: '.grid-item',
+		columnWidth: '.grid-sizer'
+	});
 });
