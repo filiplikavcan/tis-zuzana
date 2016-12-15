6fef6fc1e6349c1bc430cea1bd8f30729cdfe264
Filip Likavčan <filip@bratia.sk>
2016-12-15 18:34:59 +0100

    Release 0.8.1

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -115,7 +115,7 @@ function main($)
 		input.replaceWith(select);
 	});
 
-	if ($("#teasers-list").length > 0)
+	if ($(".teasers-list").length > 0)
 	{
         $('.teasers-list').masonry({
             // options
